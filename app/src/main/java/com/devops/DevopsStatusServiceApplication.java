f0600db2dd69d71ab56350861d909c71d5dcef2f
Himanshu <hk3072004@gmail.com>
2026-07-03 16:10:14 +0530

    feat: migrate application to Spring Boot

---
+++ app/src/main/java/com/devops/DevopsStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStatusServiceApplication.class, args);
+	}
+
+}
